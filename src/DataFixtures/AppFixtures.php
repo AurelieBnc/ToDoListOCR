c32617c2797f1434817c8dc3c3ae3669ff89eb22
Shil <aurelie.beninca@gmail.com>
2024-06-13 10:36:15 +0200

    fix style DataFixtures  + method too long

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\DataFixtures\DateFixtures;
 use App\DataFixtures\TaskFixtures;
+use App\DataFixtures\UserFixtures;
 use App\EnumTodo\TaskStatus;
 
 /**
@@ -43,45 +44,13 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager): void
     {
-        $userList = [];
 
-        // Création d'un user.
-        $firstUser = new User();
-        $firstUser->setUsername("User1");
-        $firstUser->setEmail("user1@todolist.fr");
-        $firstUser->setRoles(["ROLE_USER"]);
-        $firstUser->setPassword($this->userPasswordHasher->hashPassword($firstUser, "password"));
-        $manager->persist($firstUser);
+        $userFixture = new UserFixtures($this->userPasswordHasher);
+        $userList = $userFixture->UserList($manager);
 
-        $userList[] = $firstUser;
+        $firstUser = $userList[1];
+        $secondUser = $userList[2];
 
-        // Création d'un second user.
-        $secondUser = new User();
-        $secondUser->setUsername("User2");
-        $secondUser->setEmail("user2@todolist.fr");
-        $secondUser->setRoles(["ROLE_USER"]);
-        $secondUser->setPassword($this->userPasswordHasher->hashPassword($secondUser, "password"));
-        $manager->persist($secondUser);
-
-        $userList[] = $secondUser;
-
-        // Création d'un user sans role.
-        $otherUser = new User();
-        $otherUser->setUsername("User3");
-        $otherUser->setEmail("user3@todolist.fr");
-        $otherUser->setRoles([null]);
-        $otherUser->setPassword($this->userPasswordHasher->hashPassword($otherUser, "password"));
-        $manager->persist($otherUser);
-
-        $userList[] = $otherUser;
-        
-        // Création d'un user admin.
-        $userAdmin = new User();
-        $userAdmin->setUsername("Admin");
-        $userAdmin->setEmail("admin@todolist.fr");
-        $userAdmin->setRoles(["ROLE_ADMIN"]);
-        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
-        $manager->persist($userAdmin);
 
         // Création de 20 tasks avec Owner.
         $taskFixture = new TaskFixtures();
